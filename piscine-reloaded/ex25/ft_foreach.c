f2816ceb5aa3d5a10946c584ccf358f6964b13e2
Hsiaomeng <xiaomenghu4@gmail.com>
2025-04-05 11:55:41 +0200

    Update ft_foreach.c

--- piscine-reloaded/ex25/ft_foreach.c
+++ piscine-reloaded/ex25/ft_foreach.c
@@ -1,1 +1,27 @@
-void ft_foreach(int *tab, int length, void (*f)(int))
+#include <stdio.h>
+
+void print_int(int n)
+{
+    printf("%d ", n);
+}
+
+void double_int(int n)
+{
+    printf("%d ", n * 2);
+}
+
+int main(void)
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    
+    printf("Original array: ");
+    ft_foreach(arr, 5, &print_int);
+    printf("\n");
+    
+    printf("Doubled values: ");
+    ft_foreach(arr, 5, &double_int);
+    printf("\n");
+    
+    return 0;
+}
+
